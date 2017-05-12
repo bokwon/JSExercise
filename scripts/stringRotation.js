b66684a1ffa15af9086f5f53eac9da5576fba8f1
bo kwon <boyoung@whitesky.com>
2017-05-12 15:30:53 -0700

    modify question of stringRotation

--- scripts/stringRotation.js
+++ scripts/stringRotation.js
@@ -1,6 +1,6 @@
-//String Rotation: Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring. eg: 'waterbottle', 'erbottlewat'
+//String Rotation: Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1. eg: 'waterbottle', 'erbottlewat'
 
 // v4
-function isRotation(a, b) {
+function isSubstring(a, b) {
 	return a.length === b.length && (a+a).indexOf(b) != -1;
 }
