17e6796b9808fdebc722889e7cde438eaa94c123
bo kwon <boyoung@whitesky.com>
2017-05-26 17:16:02 -0700

    add more test cases for bigNumberFormat

--- scripts/bigNumberFormat.js
+++ scripts/bigNumberFormat.js
@@ -1,24 +1,34 @@
-function bigNumberFormat(number) {
-	
-	if (number === null || number === undefined || number === 0) {
-		return 0;
-	}
-	
-	if (number < 1000) {
-		return number;
-	}
-	
-	var numberFormat = {
-		'K': 1000,
-		'M': 1000000,
-		'B': 1000000000,
-		'T': 1000000000000
-	}
-	
-	for(var i=0; i<Object.keys(numberFormat).length; i++) {
-		var temp = Object.keys(numberFormat)[i];
-		if(number > numberFormat[temp]){
-			 return (number/numberFormat[temp]).toFixed() + temp;
-		}
-	}
+// function bigNumberFormat(number) {
+//
+// 	if (number === null || number === undefined || number === 0) {
+// 		return 0;
+// 	}
+//
+// 	if (number < 1000) {
+// 		return number;
+// 	}
+//
+// 	var numberFormat = {
+// 		'K': 1000,
+// 		'M': 1000000,
+// 		'B': 1000000000,
+// 		'T': 1000000000000
+// 	}
+//
+// 	for(var i=3; i<Object.keys(numberFormat).length; i--) {
+// 		var key = Object.keys(numberFormat)[i];
+//         var value = numberFormat[key];
+// 		if(number >= value){
+// 			return (number/value).toFixed() + key;
+// 		}
+// 	}
+// }
+
+var bigNumberFormat = function (v)
+{
+    for (var i = 0; v >= 1000; i++)
+        v = v / 1000;
+
+    var v = Math.trunc(v * 10) / 10;
+    return (v > 10 ? Math.round(v) : v) + " KMBT"[i];
 }
